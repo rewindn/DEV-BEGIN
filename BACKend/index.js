59b8f91a81f85297cf4c76a1122b7c25cfbd8d69
FARIS <farismudji@gmail.com>
2020-01-21 07:46:10 +0700

    create node js enpoint without mangodb

--- BACKend/index.js
+++ BACKend/index.js
@@ -1,6 +1,30 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000, "localhost");
-console.log('hey berhasil');
+const express = require('express');
+const app = express();
+const body = require('body-parser');
+
+let dbo = null;
+mongo.connect(dburl,(err,db)=>{
+if(err) throw err;
+dbo = db.db(namadb);
+})
+
+app.use(body.urlencoded({extended : false}))
+sonst 
+
+app.listen('8000',(b)=>{
+     console.log(b);
+  
+})
+
+app.post('/faris',(req,res)=>{
+let name = req.body.name;
+let kelas = req.body.kelas;
+res.send("nama:"+name+",kelas:"+kelas);
+})
+app.get('/faris/:nama',(req,res)=> {
+// res.send('hello world');
+
+let namav = req.params.nama;
+
+res.send("nama siswa:"+namav);
+})
